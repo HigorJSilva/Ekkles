a27f8d0c66a75cc59c0b5bd1362cc6a0bd005d02
Higor Silva <higor.junio@hotmail.com>
2022-03-09 21:30:08 -0300

    correção para informar uso de id em middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import { me, update, remove } from '../controllers/UserController';
 import { authorize } from '../middleware/Auth';
 import { validateRequest } from '../middleware/ValidateRequest';
+import { AuthenticatedUserRequest } from '../requests/AuthenticatedUserRequest';
 import { UpdateRequest } from '../requests/UserRequests';
 
 const router = express.Router()
 
 router.put('/', authorize(), validateRequest, UpdateRequest, update);     
-router.delete('/', authorize(), remove);     
-router.get('/', authorize(),  me);     
+router.delete('/', authorize(), validateRequest, AuthenticatedUserRequest, remove);     
+router.get('/', authorize(), AuthenticatedUserRequest, me);     
 
 export  {router as userRoutes};
