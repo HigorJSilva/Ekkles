25e6690c10002ffee17bcf593ab55ec9910f4714
Higor Silva <higor.junio@hotmail.com>
2022-03-18 17:33:40 -0300

    fix: listagem retorna documentos referenciados

--- src/models/VotingGroup.ts
+++ src/models/VotingGroup.ts
@@ -1,3 +1,4 @@
+import { NextFunction } from 'express';
 import mongoose, { Schema, Types, Model} from 'mongoose';
 
 interface MongoResult<T> extends mongoose.Document{
@@ -24,5 +25,14 @@ const VotingGroupSchema = new Schema<VotingGroupInterface>({
 VotingGroupSchema.statics.buildQueryParams = function (search) {
   return {nome: search}
 }
+
+const populateFields = function(this: Model<VotingGroupModelInterface>, next: NextFunction) {
+  this.populate('adminId', '_id nome email');
+  this.populate('usersId', '_id nome');
+  next();
+};
+
+//@ts-ignore
+VotingGroupSchema.pre('findOne', populateFields).pre('find', populateFields);
 
 export const VotingGroup = mongoose.model<VotingGroupInterface, VotingGroupModelInterface >('VotingGroup', VotingGroupSchema);
