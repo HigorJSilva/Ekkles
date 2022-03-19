a2933cd604c6b2c0116ca56e861115a3840e76c9
Higor Silva <higor.junio@hotmail.com>
2022-03-18 21:07:55 -0300

    feat: listagem de todos usuarios do admin e busca por id

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,10 @@
 import * as UserService from '../services/UserService';
 
 import { getFilteredRequest } from '../helpers/Utils';
-import { UpdateInterface } from '../requests/UserRequests';
+import { getUserByIdInterface, UpdateInterface } from '../requests/UserRequests';
 import { NextFunction, Request, Response } from 'express';
 import { ApiResponse } from '../helpers/Response';
+import { AuthenticatedUserInterface } from '../requests/AuthenticatedUserRequest';
 
 export async function me(req: Request, res:Response, next:NextFunction) {
 
@@ -38,6 +39,30 @@ export async function remove(req: Request, res:Response, next:NextFunction) {
         .then(user => user 
             ? (res.status(200).json( new ApiResponse(true, null, null, null)))
             : res.status(422).json( new ApiResponse(false, 'Erro ao cadastrar', null, null))
+        )
+        .catch((err: Error) => next(res.status(422).json( new ApiResponse(false, err.toString().replace('Error: ',''), null, null))));
+}
+
+export async function getAll(req: Request, res:Response, next:NextFunction) {
+
+    const filteredRequest: AuthenticatedUserInterface = <AuthenticatedUserInterface> getFilteredRequest(req);
+
+    UserService.getAll(filteredRequest.user.id)
+        .then(users => users 
+            ? (res.status(200).json( new ApiResponse(true, null, users, null)))
+            : res.status(422).json( new ApiResponse(false, 'Erro ao listar usuários', null, null))
+        )
+        .catch((err: Error) => next(res.status(422).json( new ApiResponse(false, err.toString().replace('Error: ',''), null, null))));
+}
+
+export async function getById(req: Request, res:Response, next:NextFunction) {
+
+    const filteredRequest: getUserByIdInterface = <getUserByIdInterface> getFilteredRequest(req);
+
+    UserService.getById(filteredRequest.user.id, filteredRequest.id)
+        .then(users => users 
+            ? (res.status(200).json( new ApiResponse(true, null, users, null)))
+            : res.status(422).json( new ApiResponse(false, 'Erro ao buscar usuário', null, null))
         )
         .catch((err: Error) => next(res.status(422).json( new ApiResponse(false, err.toString().replace('Error: ',''), null, null))));
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,17 @@
 import express from 'express';
-import { me, update, remove } from '../controllers/UserController';
+import { me, update, remove, getAll, getById } from '../controllers/UserController';
+import { Roles } from '../helpers/Roles';
 import { authorize } from '../middleware/Auth';
 import { validateRequest } from '../middleware/ValidateRequest';
 import { AuthenticatedUserRequest } from '../requests/AuthenticatedUserRequest';
-import { UpdateRequest } from '../requests/UserRequests';
+import { getUserByIdRequest, UpdateRequest } from '../requests/UserRequests';
 
 const router = express.Router()
 
-router.put('/', authorize(), validateRequest, UpdateRequest, update);     
-router.delete('/', authorize(), validateRequest, AuthenticatedUserRequest, remove);     
-router.get('/', authorize(), AuthenticatedUserRequest, me);     
+router.put('/', authorize(), validateRequest, UpdateRequest, update);
+router.delete('/', authorize(), validateRequest, AuthenticatedUserRequest, remove);
+router.get('/', authorize(), AuthenticatedUserRequest, me);
+router.get('/users/:id', authorize(Roles.Admin), getUserByIdRequest, getById);
+router.get('/users', authorize(Roles.Admin), AuthenticatedUserRequest, getAll);     
 
 export  {router as userRoutes};
